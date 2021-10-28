82d17d724245eef1e4a586e85b12996ce85a1238
Nedislav <nedislav1998@gmail.com>
2021-10-28 17:25:41 +0300

    Nedko first commit

--- ClientTCPMaven/src/main/java/ClientHTTP/RequestRunner.java
+++ ClientTCPMaven/src/main/java/ClientHTTP/RequestRunner.java
@@ -3,6 +3,7 @@ package ClientHTTP;
 import Config.Config;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * This class is used to run PUT or GET requests
@@ -10,10 +11,25 @@ import java.io.IOException;
 public class RequestRunner {
     public static void main(String[] args) throws IOException {
 
-        PUT put = new PUT(Config.getInstance().getPUT(), Config.getInstance().getSend_file());
-        put.putRequest(Config.getInstance().getURI());
+        Scanner in = new Scanner(System.in);
+        //Display the menu
+        System.out.println("1\t PUT");
+        System.out.println("2\t GET");
 
-        GET get = new GET();
-        //get.getRequest(Config.getInstance().getURI());
+        System.out.println("Please enter your choice:");
+
+        //Get user's choice
+        int choice=in.nextInt();
+
+        switch (choice) {
+            case 1:  PUT put = new PUT(Config.getInstance().getPUT(), Config.getInstance().getSend_file());
+                put.putRequest(Config.getInstance().getURI());
+                break;
+            case 2:  GET get = new GET();
+                get.getRequest(Config.getInstance().getURI());
+                break;
+            default: System.out.println("Invalid choice");
+        }//end of switch
+
     }
 }
